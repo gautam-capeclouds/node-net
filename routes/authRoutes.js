6352a34a5c40ef41f539f7c54aa500f7dc639811
gautam-capeclouds <capeclouds123@gmail.com>
2024-07-07 11:31:08 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('./controllers/authController');
-const jwtMiddleware = require('./middlewares/jwtMiddleware');
+const authController = require('../controllers/authController');
+const jwtMiddleware = require('../middlewares/jwtMiddleware');
 
 router.post('/token', authController.createToken);
 router.post('/check_token', jwtMiddleware, authController.checkToken);
