1a8b26d390e6c9cfb403fbef7c8c2247bbab9e64
gautam-capeclouds <capeclouds123@gmail.com>
2024-07-07 15:26:47 +0530

    Update server.js

--- functions/server.js
+++ functions/server.js
@@ -1,4 +1,4 @@
 const serverless = require('serverless-http');
-const app = require('../../app'); // Ensure the path is correct
+const app = require('./app'); // Ensure the path is correct
 
 module.exports.handler = serverless(app); // Exporting the handler correctly
