979555bcf3441edecce3398ccf14c6c735cced9c
gautam-capeclouds <capeclouds123@gmail.com>
2024-07-07 17:35:09 +0530

    Delete routes directory

--- routes/clientRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const clientController = require('../controllers/clientController');
-
-router.get('/get_user_title', clientController.getUserTitle);
-router.get('/registerDetailsUsername', clientController.getUserDetailsByUsername);
-
-module.exports = router;
